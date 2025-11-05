6025328a063fec4d0290f2b3f67c9832223066a7
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2025-11-05 17:36:59 +0100

    paquetes listos

--- src/silver_ingest.py
+++ src/silver_ingest.py
@@ -8,11 +8,17 @@ clientes = spark.read.parquet("bronze/clientes")
 ventas   = spark.read.parquet("bronze/ventas")
 fact     = spark.read.parquet("bronze/facturas_meta")
 
-ventas = ventas.withColumn("id_venta", col("id_venta").cast(IntegerType)) 
-ventas = ventas.withColumn("id_cliente", col("id_cliente").cast(IntegerType)) 
-ventas = ventas.withColumn("id_producto", col("id_producto").cast(IntegerType)) 
-ventas = ventas.withColumn("unidades", col("unidades").cast(IntegerType)) 
-ventas = ventas.withColumn("importe", col("importe").cast(DecimalType)) 
+ventas = (ventas
+.withColumn("id_venta", col("id_venta").cast(IntegerType())) 
+.withColumn("id_cliente", col("id_cliente").cast(IntegerType())) 
+.withColumn("id_producto", col("id_producto").cast(IntegerType())) 
+.withColumn("unidades", col("unidades").cast(IntegerType())) 
+.withColumn("importe", col("importe").cast(DecimalType()))
+.withColumn("fecha", to_date(col("fecha")))
+.dropDuplicates(["id_venta"])
+)
+
+ventas.write.mode("overwrite").partitionBy("fecha").parquet("silver/ventas")
 
 print("Silver listo. ")
 spark.stop()
